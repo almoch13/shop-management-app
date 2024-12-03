e149e53b9126530eb147d49b91533a443375b130
almoch13 <almoch9@gmail.com>
2024-12-03 23:22:50 +0700

    adding login and logout features

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,6 +1,10 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { createUser, findByEmail } from "../model/userModel.js";
+import {
+  createUser,
+  findByEmail,
+  findUserCredential,
+} from "../model/userModel.js";
 
 export const register = async (req, res) => {
   const { username, email, password, role } = req.body;
@@ -30,4 +34,34 @@ export const register = async (req, res) => {
       error,
     });
   }
+};
+
+export const login = async (req, res) => {
+  const { userCredential, password } = req.body;
+
+  try {
+    const user = await findUserCredential(userCredential);
+
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ message: "Login Success!", token });
+  } catch (error) {
+    res.status(500).json({
+      message: "Invalid credentials",
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.status(200).json({ message: "User logged out successfully" });
 };
